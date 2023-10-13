d23851a001e1b28cc604cbee9b08f08e7cb59b8d
Rorris <rociobarragan14@gmail.com>
2023-10-13 22:09:00 +0200

    files modified to stablish MVC

--- P2/e-commerce/etienda/models.py
+++ P2/e-commerce/etienda/models.py
@@ -1,3 +1,60 @@
 from django.db import models
 
 # Create your models here.
+from pydantic import BaseModel, FilePath, Field, EmailStr, field_serializer, field_validator
+from pymongo import MongoClient
+from pprint import pprint
+from datetime import datetime
+from typing import Any
+import requests, pathlib
+
+# https://requests.readthedocs.io/en/latest/
+def getProductos(api):
+	response = requests.get(api)
+	return response.json()
+				
+# Esquema de la BD
+# https://docs.pydantic.dev/latest/
+# con anotaciones de tipo https://docs.python.org/3/library/typing.html
+# https://docs.pydantic.dev/latest/usage/fields/
+
+class Nota(BaseModel):
+	puntuación: float = Field(ge=0., lt=5.)
+	cuenta: int = Field(ge=1)
+
+#Clase para inicializar las entradas en MongoDB			
+class Producto(BaseModel):
+	_id: Any
+	id_producto: int
+	nombre: str
+	precio: float
+	descripción: str
+	categoría: str
+	imágen: FilePath | None
+	rating: Nota
+
+	@field_serializer('imágen')
+	def serializaPath(self, val) -> str:
+		if type(val) is pathlib.PosixPath:
+			return str(val)
+		return val	
+	
+	#Valida que el nombre empiece por mayúscula
+	@field_validator('nombre')
+	@classmethod
+	def empieza_con_mayuscula(cls, v) -> str:
+		if not v[0].isupper():
+			raise ValueError("El nombre debe empezar por mayúscula.")
+		return v.title()
+	
+
+class Rate(BaseModel):
+	rate: float = Field(e=0., lt=5.)
+	count: int = Field(ge=1)
+
+
+class Compra(BaseModel):
+	_id: Any
+	usuario: EmailStr
+	fecha: datetime
+	productos: list	
--- P2/e-commerce/etienda/views.py
+++ P2/e-commerce/etienda/views.py
@@ -1,7 +1,46 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .queries import query1, query2, query3, query4, query5, query6, productos_collection, compras_collection
 
+# Create your views here
 
-# Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the etienda index.")
+    html = """
+    <body>
+        <h1>Práctica 2</h1>
+        <ol>
+            <li> <a href="C1/">Consulta 1: Electrónica entre 100 y 200€, ordenados por precio</a> </li>
+            <li> <a href="C2/">Consulta 2: Productos que contengan la palabra 'pocket' en la descripción</a> </li>
+            <li> <a href="C3/">Consulta 3: Productos con puntuación mayor de 4</a> </li>
+            <li> <a href="C4/">Consulta 4:Ropa de hombre, ordenada por puntuación </a> </li>
+            <li> <a href="C5/">Consulta 5: Facturación total</a> </li>
+            <li> <a href="C6/">Consulta 6: Facturación por categoría de producto</a> </li>
+        </ol>
+    </body>
+
+    """
+    return HttpResponse(html)
+
+def C1(request):
+    salida = query1(productos_collection)
+    return HttpResponse(salida, content_type="text/plain")
+
+def C2 (request):
+    salida = query2(productos_collection)
+    return HttpResponse(salida, content_type="text/plain")
+
+def C3 (request):
+    salida = query3(productos_collection)
+    return HttpResponse(salida, content_type="text/plain")
+
+def C4 (request):
+    salida = query4(productos_collection)
+    return HttpResponse(salida, content_type="text/plain")
+
+def C5 (request):
+    salida = query5(productos_collection, compras_collection)
+    return HttpResponse(salida, content_type="text/plain")
+
+def C6 (request):
+    salida = query6(productos_collection, compras_collection)
+    return HttpResponse(salida, content_type="text/plain") 
