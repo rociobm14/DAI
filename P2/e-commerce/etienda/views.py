f69071cb460d42a5223f409043936115ec0a0ce8
Rorris <rociobarragan14@gmail.com>
2023-10-27 17:06:59 +0200

    category view works perfectly

--- P2/e-commerce/etienda/views.py
+++ P2/e-commerce/etienda/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .queries import query1, query2, query3, query4, query5, query6, productos_collection, compras_collection, GetCategories
+from .queries import query1, query2, query3, query4, query5, query6, productos_collection, compras_collection, GetCategories, getProductsByCategory
 
 # Create your views here
 
@@ -9,6 +9,6 @@ def index(request):
     return render(request, 'etienda/index.html', context)
 
 def category(request,category):
-    context = {'categories': GetCategories(productos_collection), 'category': category}
+    context = {'products': getProductsByCategory(productos_collection, category), 'categories': GetCategories(productos_collection), 'category': category}
     return render(request, 'etienda/category.html', context)
 
