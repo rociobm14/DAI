d87cc56a7b584df3d816d6552912507039faf23e
Rorris <rociobarragan14@gmail.com>
2023-10-28 14:11:38 +0200

    views.py configured to search

--- P2/e-commerce/etienda/views.py
+++ P2/e-commerce/etienda/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .queries import query1, query2, query3, query4, query5, query6, productos_collection, compras_collection, GetCategories, getProductsByCategory
-
-# Create your views here
+from .queries import query1, query2, query3, query4, query5, query6, productos_collection, compras_collection, GetCategories, getProductsByCategory, SearchProducts
 
 def index(request):
     context = {'products': productos_collection.find(), 'categories': GetCategories(productos_collection)}
     return render(request, 'etienda/index.html', context)
+
+def search(request):
+    context = {'search': request.GET.get('to_find', ''), 'products': SearchProducts(productos_collection, request.GET.get('to_find', ''))}
+    return render(request, 'etienda/search.html', context)
 
 def category(request,category):
     context = {'products': getProductsByCategory(productos_collection, category), 'categories': GetCategories(productos_collection), 'category': category}
