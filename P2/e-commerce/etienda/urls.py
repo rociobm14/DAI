0151b7adcc49a93d5d72f3bafee02d2c7dfd8e28
Rorris <rociobarragan14@gmail.com>
2023-10-28 14:07:44 +0200

    url to search configured

--- P2/e-commerce/etienda/urls.py
+++ P2/e-commerce/etienda/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     
     path("", views.index, name="index"),
+    path('search/', views.search, name='search'),
     path('categories/<str:category>/', views.category, name='category'),
     
 ]
