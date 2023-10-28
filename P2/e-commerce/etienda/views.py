3cedffdb070451702c87c18eb16a0af495ef465c
Rorris <rociobarragan14@gmail.com>
2023-10-28 15:02:44 +0200

    search configured to appear the categorie's menu

--- P2/e-commerce/etienda/views.py
+++ P2/e-commerce/etienda/views.py
@@ -3,14 +3,19 @@ from django.http import HttpResponse
 from .queries import query1, query2, query3, query4, query5, query6, productos_collection, compras_collection, GetCategories, getProductsByCategory, SearchProducts
 
 def index(request):
-    context = {'products': productos_collection.find(), 'categories': GetCategories(productos_collection)}
+    context = {'products': productos_collection.find(),
+               'categories': GetCategories(productos_collection)}
     return render(request, 'etienda/index.html', context)
 
 def search(request):
-    context = {'search': request.GET.get('to_find', ''), 'products': SearchProducts(productos_collection, request.GET.get('to_find', ''))}
+    context = {'search': request.GET.get('to_find', ''), 
+               'products': SearchProducts(productos_collection, request.GET.get('to_find', '')),
+               'categories': GetCategories(productos_collection)}
     return render(request, 'etienda/search.html', context)
 
 def category(request,category):
-    context = {'products': getProductsByCategory(productos_collection, category), 'categories': GetCategories(productos_collection), 'category': category}
+    context = {'products': getProductsByCategory(productos_collection, category), 
+               'categories': GetCategories(productos_collection), 
+               'category': category}
     return render(request, 'etienda/category.html', context)
 
