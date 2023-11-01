8fe146dd8948dd754dd5b89b948beed03fe96066
Rorris <rociobarragan14@gmail.com>
2023-11-01 15:00:54 +0100

    view to add product + image handling

--- P2/e-commerce/etienda/views.py
+++ P2/e-commerce/etienda/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .queries import query1, query2, query3, query4, query5, query6, productos_collection, compras_collection, GetCategories, getProductsByCategory, SearchProducts
+from .queries import query1, query2, query3, query4, query5, query6, productos_collection, compras_collection, GetCategories, getProductsByCategory, SearchProducts, getLastProductID
+from .forms import ProductoForm
+from .models import Producto
+from pydantic import FilePath
 
 def index(request):
     context = {'products': productos_collection.find(),
@@ -20,3 +23,39 @@ def category(request,category):
                'category': category}
     return render(request, 'etienda/category.html', context)
 
+def newproduct(request):
+    form = ProductoForm()
+    if request.method == 'POST':
+        form = ProductoForm(request.POST, request.FILES)
+        if form.is_valid():
+            nombre_archivo = handle_uploaded_file(request.FILES['imágen'])
+            #construyo el producto
+            p = {
+                "id_producto": getLastProductID(productos_collection)+1,
+                "nombre": form.cleaned_data["nombre"],
+                "precio": form.cleaned_data["precio"],
+                "descripción": form.cleaned_data["descripción"],
+                "categoría": form.cleaned_data["categoría"],
+                "imágen": "imagenes/" + nombre_archivo,
+                "rating": {'puntuación':0.0, 'cuenta': 1.0}
+            }
+            product = Producto(**p)
+            
+            #Inserta el producto en la BD
+            productos_collection.insert_one(product.model_dump())
+            
+            return redirect('index')
+        
+    context = {'id': getLastProductID(productos_collection),
+               'form': form,
+               'categories': GetCategories(productos_collection)}
+    return render(request, 'etienda/newproduct.html', context)
+            
+            
+def handle_uploaded_file(f):
+    path = "imagenes/" + f.name
+    with open(path, "wb+") as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
+    return f.name
+    
