1378c9f847a120cfbc75b5afdb1035bdf47d652b
Rorris <rociobarragan14@gmail.com>
2023-11-01 15:04:01 +0100

    settings configured

--- P2/e-commerce/Ecommerce/settings.py
+++ P2/e-commerce/Ecommerce/settings.py
@@ -124,3 +124,52 @@ STATIC_URL = 'imagenes/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'imagenes/')]
+
+LOG_FILE = 'Server.log'
+
+LOGGING = {
+	'version': 1,
+	'disable_existing_loggers': False,
+								
+	'formatters': {
+
+		'verbose': {
+			'format': "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+									'datefmt': "%d/%b/%Y %H:%M:%S"
+			},
+
+		'simple': {
+			'format': '%(levelname)s [%(name)s:%(lineno)s] %(message)s'
+			},
+		},
+								
+	'handlers': {
+
+		'file': {
+			'level': 'INFO',
+			'class': 'logging.FileHandler',
+			'filename': BASE_DIR / LOG_FILE,
+			'formatter': 'verbose',
+			'mode': 'w'   # ?? sobre escribe cada vez
+			},
+
+		'console': {
+			'level': 'DEBUG',
+			'class': 'logging.StreamHandler',
+			'formatter': 'simple'
+		}
+	},
+								
+	'loggers': {     # interno de django
+		'django': {
+			'handlers': ['file'],
+			'propagate': True,   
+			'level': 'ERROR',
+		},
+
+		'': {  # los demás en cada archivo  ??
+				'handlers': ['file', 'console'],
+				'level': 'DEBUG',
+			}
+		}
+	}
